57cd932fc50c38edee33c1ffd6202ee6bfa0ca15
Jamie Bort <jamiebort@gmail.com>
2022-02-28 11:18:22 -0500

    Moved all the files from the 03-task-manager-with-React-front-end/ directory to the back-end/ directory.

---
+++ back-end/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+// middleware
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+// routes
+
+// app.get('/hello', (req, res) => {
+// 	res.send('Task Manger App');
+// });
+
+app.use('/api/v1/tasks', tasks);
+
+// const port = 3000; // original port
+const port = 5000; // using this one temporarily since the front end is using port 3000
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		console.log('Connecting to database...');
+		app.listen(port, console.log(`Server is listening on port ${port}...`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+start();
